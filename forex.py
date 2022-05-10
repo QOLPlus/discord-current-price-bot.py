27beabe444e33610062e908bb1f4ebfb7c0647b4
LEE Jaeyoung <jaeyoung@monodiary.net>
2022-05-10 11:25:33 +0900

    💰

---
+++ forex.py
@@ -0,0 +1,48 @@
+import disnake
+from disnake.ext import tasks
+
+import os
+import requests
+
+class Scheduler(disnake.Client):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.last_price = None
+        self.interval = os.getenv('INTERVAL') or 15
+        self.short_code = os.getenv('SHORT_CODE')
+
+    async def on_ready(self):
+        print(f"Logged in as {self.user} (ID: {self.user.id})")
+        print(f"SHORT_CODE: {self.short_code}")
+        print(f"INTERVAL: {self.interval}")
+        print("------")
+
+    async def on_connect(self):
+        self.fetch_price.start()
+    
+    @tasks.loop(seconds=int(os.getenv('INTERVAL') or 15))
+    async def fetch_price(self):
+        await self.wait_until_ready()
+
+        r = requests.get(f"https://quotation-api-cdn.dunamu.com/v1/forex/recent?codes={self.short_code}")
+        try:
+            data = r.json()[0]
+
+            stock_name = data['name']
+            display_name = f"{stock_name}"
+            trade_price = data['basePrice']
+            price_detail = f"({data['date']})"
+
+            if self.user.display_name != display_name:
+                await self.user.edit(username=display_name)
+
+            if self.last_price != trade_price:
+                await self.change_presence(activity=disnake.Game(name=f"💰 {trade_price:,} {price_detail}"))
+                self.last_price = trade_price
+
+        except Exception as e:
+            print(e)
+            pass
+
+scheduler = Scheduler()
+scheduler.run(os.environ.get('DISCORD_BOT_TOKEN'))
